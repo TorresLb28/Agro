c087ad92f99dd50a45ce9ada9fbe25393a788819
Edgar Torres <edgarlb28@outlook.com>
2024-01-24 10:10:42 -0600

    arquitectura de proyecto agro

---
+++ Agro/Agro.web/Data/Entities/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+    {
+        //ETN
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+    }
+}
